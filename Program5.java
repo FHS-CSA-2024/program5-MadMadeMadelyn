dd1f33eea4688bf1e324c0b0d873a91f8ec7aa59
MadMadeMadelyn <madelynching916@gmail.com>
2024-08-20 14:42:27 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -2,6 +2,32 @@
 
 //Your code here
 
+public class Program5 {
+    public static void main(String[] args){
+        int Rm = 286;
+        int Rg = 9;
+        double Ra = (double)Rm/Rg;
+        
+        int KKm = 412;
+        int KKg = 40;
+        double KKa = (double)KKm/KKg;
+        
+        int PFm = 361;
+        int PFg = 18;
+        double PFa = (double)PFm/PFg;
+        
+        int Bm = 161;
+        int Bg = 11;
+        double Ba = (double)Bm/Bg;
+        
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon: ");
+        System.out.println("Royale averaged " + Ra + " m/g");
+        System.out.println("Koopa King averaged " + KKa + " m/g");
+        System.out.println("Pipe Frame averaged " + PFa + " m/g");
+        System.out.println("Badwagon averaged " + Ba + " m/g");
+    }
+}
+
 //Paste console output below:
 /*
 
