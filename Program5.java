652a2b1f4f37d16482ff36b75adc5321b02bfc0c
MadMadeMadelyn <madelynching916@gmail.com>
2024-08-20 15:15:04 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -7,7 +7,10 @@ public class Program5 {
         int Rm = 286;
         int Rg = 9;
         double Ra = (double)Rm/Rg;
-        
+        Ra = Ra * 10;
+        Ra = (double) ((int)Ra);
+        Ra = Ra/10;
+    
         int KKm = 412;
         int KKg = 40;
         double KKa = (double)KKm/KKg;
@@ -15,10 +18,16 @@ public class Program5 {
         int PFm = 361;
         int PFg = 18;
         double PFa = (double)PFm/PFg;
+        PFa = PFa * 10;
+        PFa = (double) ((int)PFa);
+        PFa = PFa/10;
         
         int Bm = 161;
         int Bg = 11;
         double Ba = (double)Bm/Bg;
+        Ba = Ba*10;
+        Ba = (double) ((int)Ba);
+        Ba = Ba/10;
         
         System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon: ");
         System.out.println("Royale averaged " + Ra + " m/g");
